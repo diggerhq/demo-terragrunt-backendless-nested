9c41a25faaeb8e2796879151b61ff4830518cc10
motatoes <moe.habib9@gmail.com>
2024-03-25 11:32:25 +0000

    commit modules

--- cloud/projectD/nonprod/dev-svc-identity/main.tf
+++ cloud/projectD/nonprod/dev-svc-identity/main.tf
@@ -1,10 +1,10 @@
 module "iam_role" {
-  source    = "../modules/iam_role"
+  source    = "../../modules/iam_role"
   role_name = "s3_bucket_role_identity"
 }
 
 module "s3_bucket" {
-  source       = "../modules/s3_bucket"
+  source       = "../../modules/s3_bucket"
   bucket_name  = "demo-terragrunt-parallel-svc-identity"
   iam_role_arn = module.iam_role.role_arn
 }
--- cloud/projectD/nonprod/dev-svc-messaging/main.tf
+++ cloud/projectD/nonprod/dev-svc-messaging/main.tf
@@ -1,10 +1,10 @@
 module "iam_role" {
-  source    = "../modules/iam_role"
+  source    = "../../modules/iam_role"
   role_name = "s3_bucket_role_messaging"
 }
 
 module "s3_bucket" {
-  source       = "../modules/s3_bucket"
+  source       = "../../modules/s3_bucket"
   bucket_name  = "demo-terragrunt-parallel-svc-messaging"
   iam_role_arn = module.iam_role.role_arn
 }
